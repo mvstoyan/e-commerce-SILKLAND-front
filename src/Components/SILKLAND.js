1d41c417e064d37c6760a84b098222da585e81f2
Marina Tilton <m.v.stoyan@gmail.com>
2023-06-29 19:15:11 -0700

    photo

--- src/Components/SILKLAND.js
+++ src/Components/SILKLAND.js
@@ -5,23 +5,9 @@ function SILKLAND() {
 
     return (
       <div className="SILKLAND">
-            <img className="floral" src="https://drive.google.com/uc?id=1kYLw1tFsn76ieG3V32csf3470vbOaHIX" alt="floral"/>
+            <img className="floral" src="./floralWhiteBlack0.webp" alt="floral"/>
             <Link to="/Shop"><button className="btn">shop now</button></Link>
       </div>
-        /*
-      <div className="two">
-        <img className="abstarctBlack0" src="./content/image/abstractBlack/abstractBlack0.png" alt="abstractBlack0"/>
-      </div>
-      <div className="three">
-      </div>
-      <div className="start">
-      <div className="container">
-        <video autoPlay muted loop>
-          <source src="/content/video/floralWhiteBlack.mp4" type="video/mp4" />
-        </video>
-        <h1>SilkLand</h1>
-      </div>
-      </div>*/
     );
 }
 
